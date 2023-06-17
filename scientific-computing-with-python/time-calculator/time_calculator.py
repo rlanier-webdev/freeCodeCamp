b89cf2add52ac9004d1abe579f43fcd74b707a37
RLW <developer@rashundawilliams.com>
2023-06-17 04:06:40 -0500

    update

--- scientific-computing-with-python/time-calculator/time_calculator.py
+++ scientific-computing-with-python/time-calculator/time_calculator.py
@@ -1,16 +1,25 @@
 '''
-https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator
+FreeCodeCamp's Scientific Computing with Python - Time Calculator
 
-    Write a function named add_time that takes in two required parameters and one optional parameter:
+Write a function named add_time that takes in two required parameters and one optional parameter:
 
-    a start time in the 12-hour clock format (ending in AM or PM)
-    a duration time that indicates the number of hours and minutes
-    (optional) a starting day of the week, case insensitive
+a start time in the 12-hour clock format (ending in AM or PM)
+a duration time that indicates the number of hours and minutes
+(optional) a starting day of the week, case insensitive
 
-    The function should add the duration time to the start time and return the result.
+The function should add the duration time to the start time and return the result.
 
-    If the result will be the next day, it should show (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
+If the result will be the next day, it should show (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
 
-    If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result. The day of the week in the output should appear after the time and before the number of days later.
-    
+If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result. The day of the week in the output should appear after the time and before the number of days later.
+
+Author: RaShunda Williams
+Completed: 06-17-23
 '''
+
+def add_time(start, duration, day):
+
+    # The function should add the duration time to the start time 
+
+    # return the result.
+    return ''
