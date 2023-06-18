3f383b0911d9fe01c5e5703afe2a10893a0e8a18
RLW <developer@rashundawilliams.com>
2023-06-18 00:30:16 -0500

    WIP: working on (next day) condition

--- scientific-computing-with-python/time-calculator/time_calculator.py
+++ scientific-computing-with-python/time-calculator/time_calculator.py
@@ -1,25 +1,40 @@
 '''
 FreeCodeCamp's Scientific Computing with Python - Time Calculator
 
-Write a function named add_time that takes in two required parameters and one optional parameter:
-
-a start time in the 12-hour clock format (ending in AM or PM)
-a duration time that indicates the number of hours and minutes
-(optional) a starting day of the week, case insensitive
-
-The function should add the duration time to the start time and return the result.
-
-If the result will be the next day, it should show (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
-
-If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result. The day of the week in the output should appear after the time and before the number of days later.
+Write a function named add_time that takes in two required parameters and one optional parameter.
 
 Author: RaShunda Williams
 Completed: 06-17-23
 '''
 
-def add_time(start, duration, day):
+# imports
+from datetime import datetime, timedelta
 
-    # The function should add the duration time to the start time 
+# a start time in the 12-hour clock format (ending in AM or PM)
+start = '1:00 PM'
 
-    # return the result.
-    return ''
+# a duration time that indicates the number of hours and minutes
+duration = '03:05'
+
+# (optional) a starting day of the week, case insensitive
+day = ''
+
+def add_time(start, duration):
+    # Parse the time string into a datetime object
+    start = datetime.strptime(start,'%I:%M %p')
+
+    # convert duration from string to time
+    h, m = duration.split(':')
+    duration = timedelta(hours=int(h),minutes=int(m))
+    
+    # add the duration time to the start time 
+    result = start + duration
+
+    # If the result will be the next day, it should show (next day) after the time. 
+    # If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
+    # If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result.
+    # The day of the week in the output should appear after the time and before the number of days later.
+
+    return result
+
+print(add_time(start,duration))
