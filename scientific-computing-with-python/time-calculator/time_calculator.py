813db2d65b0deebfe787bf669dca9cefccb4a453
RaShunda Lanier <developer@rashundawilliams.com>
2023-06-19 23:39:15 -0500

    wip: nextday
    Check if the end time is on the next day

--- scientific-computing-with-python/time-calculator/time_calculator.py
+++ scientific-computing-with-python/time-calculator/time_calculator.py
@@ -8,13 +8,7 @@ Completed: 06-17-23
 '''
 
 # imports
-from datetime import datetime, timedelta
-
-# a start time in the 12-hour clock format (ending in AM or PM)
-start = '11:00 PM'
-
-# a duration time that indicates the number of hours and minutes
-duration = '03:05'
+from datetime import datetime, time, timedelta
 
 # (optional) a starting day of the week, case insensitive
 day = ''
@@ -31,14 +25,18 @@ def add_time(start, duration):
     result = start + duration
 
     # If the result will be the next day, it should show (next day) after the time.
-    # convert result to 12 oclock format
-    # if pm to am then next day
-
+    if result.day != start.day:
+        print('Next Day!!!')
 
     # If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
+    #days_diff = (result - start).days
+
+    # if days_diff > 0:
+    #     print(f'{days_diff} days later')
     # If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result.
     # The day of the week in the output should appear after the time and before the number of days later.
 
+    #return result.strftime("%I:%M %p")
     return result
 
-print(add_time(start,duration))
+print(add_time('11:00 PM','43:05'))
